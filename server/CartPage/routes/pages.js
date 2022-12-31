b8dec67cff413dd6ae7697cceb751ed50e9525e6
Md-Alqma <mdalqma7@gmail.com>
2022-12-31 19:24:09 +0530

    completed the routing part

--- server/CartPage/routes/pages.js
+++ server/CartPage/routes/pages.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.send("Working");
+    res.send("Route is Working");
 });
   
-//Exports
+//Exports from pages
 
 module.exports = router;
